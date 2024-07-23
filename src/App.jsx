21df3dd9b95ec5a7e06e828b78a2fd6586c21bab
jihun <djawlgns7@gmail.com>
2024-07-23 20:55:58 +0900

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Route, BrowserRouter as Router, Routes} from "react-router-dom";
 import Home from "./components/Home.jsx";
 import ModalExample from "./components/test/ModalExample.jsx";
 import {ModalProvider} from "./components/context/ModalContext.jsx";
+import ChatPrototype from "./components/chat/ChatPrototype.jsx";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
 
                     {/*    테스트 컴포넌트를 라우팅하는 부분입니다*/}
                     <Route path="/test/modal" element={<ModalExample/>}/>
+                    <Route path="/test/chat" element={<ChatPrototype/>}/>
                 </Routes>
             </Router>
         </ModalProvider>
