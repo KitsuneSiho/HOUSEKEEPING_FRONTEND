3f952bcacdde23491c5193d16ab0a7103a89b03f
jihun <djawlgns7@gmail.com>
2024-08-07 14:04:57 +0900

    1대1 채팅 중복 방 생성 막음 친구와 채팅 방 이미지 제대로 나옴 1대1 채팅 방 이름 제대로 나옴

--- src/router/ChatRouter.jsx
+++ src/router/ChatRouter.jsx
@@ -3,7 +3,7 @@ import {Route, Routes} from "react-router-dom";
 import {SocketProvider} from "../components/context/SocketContext.jsx";
 import ChatLogin from "../pages/chat/ChatLogin.jsx";
 import ChatRoomList from "../pages/chat/ChatRoomList.jsx";
-import ChatRooms from "../pages/chat/ChatRooms.jsx";
+import ChatRoomPage from "../pages/chat/ChatRoomPage.jsx";
 import CreateChat from "../pages/chat/CreateChat.jsx";
 
 const ChatRouter = () => {
@@ -17,7 +17,7 @@ const ChatRouter = () => {
                     <Routes>
                         <Route path="/login" element={<ChatLogin/>}/>   {/*임시로 만든 채팅 로그인*/}
                         <Route path="/" element={<ChatRoomList/>}/>     {/*채팅 방 리스트*/}
-                        <Route path="/:chatRoomId" element={<ChatRooms/>}/>      {/*채팅 방 상세*/}
+                        <Route path="/:chatRoomId" element={<ChatRoomPage/>}/>      {/*채팅 방 상세*/}
                         <Route path="/create" element={<CreateChat/>}/>         {/*채팅 방 생성*/}
                     </Routes>
                 </ModalProvider>
