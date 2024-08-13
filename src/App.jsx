f427402cb9b356ddca0c51ff1e88edc020f52feb
boddak <kbh000111@naver.com>
2024-08-13 18:26:38 +0900

    관리자 페이지 제작중..

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import EditRoom from "./pages/room/EditRoom.jsx";
 import Test from "./pages/Test.jsx";
 import {RoutineProvider} from "./contexts/RoutineContext.jsx";
 import {SocketProvider} from "./contexts/SocketContext.jsx";
+import Manager from "./jsx/fix/Manager.jsx";
 
 function App() {
     return (
@@ -68,6 +69,8 @@ function App() {
                             <Route path="/test/myroom" element={<MyRoom/>}/>
                             <Route path="/test/myroom/edit" element={<EditRoom/>}/>
                             <Route path="/test" element={<Test/>}/>
+
+                            <Route path="/manager" element={<Manager/>}/>
                         </Routes>
                     </Router>
                 </RoutineProvider>
