799852eec178771a16d280a622af7f84aeba0fdc
djawlgns7 <djawlgns7@gmail.com>
2024-08-19 12:01:28 +0900

    백과 소켓 url 변경

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
+import {BACK_URL} from "../Constraints.js";
 
-const BACK_URL = 'http://localhost:8080';
 
 const apiClient = axios.create({
     baseURL: BACK_URL,
@@ -17,7 +17,6 @@ apiClient.interceptors.request.use(
         return config;
     },
     (error) => {
-        console.error('Request interceptor error:', error);
         return Promise.reject(error);
     }
 );
@@ -33,26 +32,17 @@ apiClient.interceptors.response.use(
                 // 액세스 토큰 재발급 요청
                 const response = await axios.post(`${BACK_URL}/reissue`, {}, {withCredentials: true});
 
-                console.log("All Headers:", response.headers);
                 const Authorization = response.headers['authorization'];
-                console.log("Authorization Header:", Authorization);
 
-                const newAccessToken = Authorization ? Authorization.split(' ')[1] : null;
+                const newAccessToken = Authorization.split(' ')[1];
 
-                console.log("newAccessToken", newAccessToken);
+                localStorage.setItem('access', newAccessToken);
 
-                if (newAccessToken) {
-                    localStorage.setItem('access', newAccessToken);
+                // 재시도할 요청에 새로운 토큰 적용
+                originalRequest.headers['Authorization'] = Authorization;
 
-                    // 재시도할 요청에 새로운 토큰 적용
-                    originalRequest.headers['Authorization'] = Authorization;
-
-                    // 원래의 요청을 재시도
-                    return apiClient(originalRequest);
-                } else {
-                    console.error("Failed to get new access token");
-                    throw new Error("Token refresh failed");
-                }
+                // 원래의 요청을 재시도
+                return apiClient(originalRequest);
             } catch (refreshError) {
                 // 재발급 실패 시 토큰 삭제 및 로그인 페이지로 리다이렉트
                 // localStorage.removeItem('access');
